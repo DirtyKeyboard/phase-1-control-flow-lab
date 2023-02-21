55608ae1a34137b3ca5ee53ab96df5e208f4a0d9
Andrew Hawileh <andrewh829@gmail.com>
2023-02-21 10:56:18 -0600

    Complete

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(f){
+  switch (true)
+  {
+    case f < 400: return 'This one is on me!';
+    case f <= 2000: return 'That will be twenty bucks.';
+    case f < 2500: return 'I will gladly take your thirty bucks.';
+    case f > 2500: return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(c){
+  switch (c)
+  {
+    case "NYC": return 'Ok, sounds good.'
+    default: return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(t){
+  switch (t)
+  {
+  case 'generous': return 'Thank you so much.';
+  case 'not as generous': return 'Thank you.';
+  case 'thanks for everything': return 'Bye.';
+  }
 }
